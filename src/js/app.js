8206f9cee7c93cf355c7fd39abac0cfd151f56b7
Michał Lach <kontakt@michal-lach.pl>
2016-05-28 00:05:22 +0200

    project structure

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+
+const App = (props) => {
+  return <h1>hello4</h1>
+}
+
+ReactDOM.render( <App /> ,
+  document.getElementById('app')
+)
